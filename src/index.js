1dfaa82d3e5dfccfad678526121c8d477bdda1a8
Klimm Ivan <ivanklimm87@gmail.com>
2023-11-02 10:29:24 +0300

    feat: Added test web site

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import './assets/logo.svg';
 import './assets/viber.png';
 import './assets/whatsapp.png';
 import './assets/panel.jpg';
+import closeFeedBack from './js/fc';
 import './styles/style.sass';
+
+
+closeFeedBack()
